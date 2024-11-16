69646ea76ea6db839844edce9eb5209d8e180c24
Rohit <90521597+Rohit311098@users.noreply.github.com>
2024-11-16 20:17:24 +0530

    Pattern2.java

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,22 @@
+Pattern:
+Input:  ‘N’ = 3
+
+Output: 
+* 
+* *
+* * *
+
+
+public class Solution {
+    public static void nForest(int n) {
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
