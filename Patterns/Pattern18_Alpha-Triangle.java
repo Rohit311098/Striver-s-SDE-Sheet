860b1f46d604fe334319bce5541679e7df124fc3
Rohit <90521597+Rohit311098@users.noreply.github.com>
2024-11-22 23:13:29 +0530

    Update Pattern18_Alpha-Triangle.java

--- Patterns/Pattern18_Alpha-Triangle.java
+++ Patterns/Pattern18_Alpha-Triangle.java
@@ -8,3 +8,21 @@ C
 C B 
 C B A
 
+
+  public class Solution {
+    public static void alphaTriangle(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++)
+        {
+            char alpha=(char)('A'+n-1);
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(alpha+" ");
+                alpha--;
+            }
+            System.out.println();
+
+        }
+    }
+}
+
