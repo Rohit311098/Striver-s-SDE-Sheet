4d45757be7cea21e6a2c4126cffd97242798761d
Rohit <90521597+Rohit311098@users.noreply.github.com>
2024-12-29 14:11:58 +0530

    Update Ceil The Floor.java
    Both Floor and Ceil value

--- Binary Search 1 D/Ceil The Floor.java
+++ Binary Search 1 D/Ceil The Floor.java
@@ -1,3 +1,50 @@
 // GFG- Ceil The Floor
 
-
+class Solution {
+    
+    public int getfloor(int[] arr,int x)
+    {
+        int floor=-1;
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(arr[mid]<=x) 
+            {
+                floor=arr[mid];
+                left=mid+1;
+            }
+            else right=mid-1;
+        }
+        return floor;
+    }
+    
+    public int getceil(int[] arr,int x)
+    {
+        
+        int ceil=-1;
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(arr[mid]>=x) 
+            {
+                ceil=arr[mid];
+                right=mid-1;
+            }
+            else left=mid+1;
+        }
+        return ceil;
+        
+    }
+    public int[] getFloorAndCeil(int x, int[] arr) {
+        
+        Arrays.sort(arr);
+        int[] result=new int[2];
+        result[0]=getfloor(arr,x);
+        result[1]=getceil(arr,x);
+        return result;
+    }
+}
