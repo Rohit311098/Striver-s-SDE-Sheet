d38212de7ff12aef06b2f8f5a936056cd0b36e9d
Rohit <90521597+Rohit311098@users.noreply.github.com>
2025-01-06 21:19:56 +0530

    Update 1021. Remove Outermost Parentheses.java
    Without using stack

--- String/Basic Questions/1021. Remove Outermost Parentheses.java
+++ String/Basic Questions/1021. Remove Outermost Parentheses.java
@@ -3,25 +3,25 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         String result="";
-        Stack<Character> stk=new Stack<>();
+        int c=0;
 
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='(')
             {
                 //This check ensure that i will not add outermost opening bracket
-                if(stk.size()!=0)
+                if(c!=0)
                 {
                     result+="(";
                 }
-                stk.add('(');
+                c++;
             }
             else
             {
                 //First we remove the opening bracket 
-                stk.pop();
+                c--;
                 //If size become enmpty means it was outermost else it means was not outermost
-                if(stk.size()!=0)
+                if(c!=0)
                 {
                     result+=")";
                 }
